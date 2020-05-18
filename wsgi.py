6e79b8beb62e8a6162dc8601cbcccd42736a3dc2
helixliu <34231528+helixliu@users.noreply.github.com>
2020-05-18 20:07:57 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -20,15 +20,11 @@ def hello():
 @application.route('/connect', methods=['GET', 'POST'])
 def connect():
     if request.method == 'GET':
-        query_string = request.query_string
-        query_list = query_string.split('&')
-        b = {}
-        for i in query_list:
-            b[i.split('=')[0]] = i.split('=')[1]
-
+        args = request.args
+        print(args)
         try:
-            check_signature(token='001001001001', signature=b['signature'], timestamp=b['timestamp'], nonce=b['nonce'])
-            resp_body = (b['echostr'])
+            check_signature(token='001001001001', signature=args['signature'], timestamp=args['timestamp'], nonce=args['nonce'])
+            resp_body = (args['echostr'])
         except InvalidSignatureException:
             pass
         return resp_body
